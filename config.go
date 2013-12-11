15760051c347968eea5a1099487a447d1dc6c5ef
DmitriKo <dmitrikozhevin@gmail.com>
2013-12-11 22:54:58 +0800

    hande config data in TOML, YAML and JSON formats

--- config.go
+++ config.go
@@ -4,35 +4,50 @@ package ma
 
 import (
 	"encoding/json"
-//	"errors"
+	//	"errors"
 	"github.com/BurntSushi/toml"
+	yaml "github.com/gonuts/yaml"
 	"strings"
+	"log"
+//	"fmt"
 )
 
 type ClusterConfig struct {
-	Name        string
-	Description string
-	Hosts       []HostConfig
+	Name        string `yaml:"name" json:"name" toml:"name"`
+	Description string `yaml:"description" json:"description" toml:"description"`
+	Hosts       []HostConfig `yaml: "hosts" json:"hosts" toml:"hosts"`
 }
 
 type HostConfig struct {
-	RemoteIp string `toml:"remote_ip"`
+	RemoteIp string `toml:"remote_ip" yaml:"remote_ip" json:"remote_ip"`
 }
 
-func NewClusterConfig(data string) (error, *ClusterConfig) {
+func NewClusterConfig(data string) (err error, config *ClusterConfig) {
 	data = strings.TrimSpace(data)
-	var config *ClusterConfig
+
 	if strings.HasPrefix(data, "{") {
-		err := json.Unmarshal([]byte(data), &config)
+		err = json.Unmarshal([]byte(data), &config)
 		if err != nil {
 			return err, nil
 		}
-		return nil, config
+		log.Print("loaded JSON config")
+		return 
 	}
-	_, err := toml.Decode(data, &config)
+
+	if strings.HasPrefix(data, "---") {
+		err = yaml.Unmarshal([]byte(data), &config)
+		if err != nil {
+			return err, nil
+		}
+		log.Print("loaded YAML config")
+		return 
+	}
+
+	_, err = toml.Decode(data, &config)
 	if err != nil {
 		return err, nil
 	}
+	log.Print("loaded TOML config")
 
-	return nil, config
+	return
 }
--- config_test.go
+++ config_test.go
@@ -8,31 +8,42 @@ import (
 
 const (
 	JSON_CONFIG = `
-{"Name": "TestCluster",
-"Description": "Test Cluster",
-"Hosts": [{"RemoteIp":"127.0.0.1"}]
+{"name": "TestCluster",
+"description": "Test Cluster",
+"hosts": [{"remote_ip":"127.0.0.1"}]
 }`
 	TOML_CONFIG = `
-Name = "TestCluster"
-Description = "Test Cluster"
+name = "TestCluster"
+description = "Test Cluster"
 [[hosts]]
 remote_ip = "127.0.0.1"
+`
+
+	YAML_CONFIG = `
+---
+name: TestCluster
+description: Test Cluster
+hosts:
+  - remote_ip: 127.0.0.1
 `
 )
 
 func TestConfigNew(t *testing.T) {
-	for _, data := range []string{JSON_CONFIG, TOML_CONFIG} {
+	for _, data := range []string{JSON_CONFIG, TOML_CONFIG, YAML_CONFIG} {
 		err, config := NewClusterConfig(data)
 		if err != nil {
 			t.Error(err)
 			continue
 		}
 		if config.Name != "TestCluster" {
-			t.Errorf("expect %s for config.Name, got `%s`", "TestCluster", config.Name)
+			t.Errorf("expect %s for config.Name, got `%s` from %s", 
+				"TestCluster", config.Name, data)
+			t.Errorf("%v", config)
 			continue
 		}
 		if config.Hosts[0].RemoteIp != "127.0.0.1" {
-			t.Errorf("expect %s for config.Hosts[0].RemoteIp, got '%s'", "127.0.0.1", config.Hosts[0].RemoteIp)
+			t.Errorf("expect %s for config.Hosts[0].RemoteIp, got '%s' from %s", 
+				"127.0.0.1", config.Hosts[0].RemoteIp, data)
 		}
 	}
 }
